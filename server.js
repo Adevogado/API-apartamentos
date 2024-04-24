0f0733791e238b6023f03d519e809d6bed78470f
Rafael S. <rafael.silveira@artefatodigital.com>
2024-04-24 19:42:27 -0300

    teste alterando o database para firebase

--- server.js
+++ server.js
@@ -3,183 +3,118 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, child, get } = require("firebase/database");
 
-// Aumentar o limite de tamanho do payload para 15MB (ou o tamanho que você desejar)
-app.use(bodyParser.json({ limit: "15mb" }));
-
-app.use(cors());
-
-// Configurações do banco de dados MySQL
-const dbConfig = {
-  host: "871.h.filess.io",
-  user: "apartmentDB_soilstrip",
-  password: "cc3ba97d47d28fa951bcdd87dcb1f8f676386d63",
-  database: "apartmentDB_soilstrip",
-  port: 3307,
+// Configurações do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCf6_ujKoemWLteg8akgUY3vFhkiGa4mQE",
+  authDomain: "apartfind-1df62.firebaseapp.com",
+  databaseURL: "https://apartfind-1df62-default-rtdb.firebaseio.com",
+  projectId: "apartfind-1df62",
+  storageBucket: "apartfind-1df62.appspot.com",
+  messagingSenderId: "69937993251",
+  appId: "1:69937993251:web:7d6ba8af6cd53e33258396",
+  measurementId: "G-HNEYBR4247",
 };
 
-// Cria uma conexão com o banco de dados MySQL
-const db = mysql.createConnection(dbConfig);
+// Inicializa o Firebase
+const appFirebase = initializeApp(firebaseConfig);
+const database = getDatabase(appFirebase);
 
-// Conecta ao banco de dados MySQL
-db.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
-    return;
+app.use(bodyParser.json({ limit: "15mb" }));
+app.use(cors());
+
+app.get("/apartamentos", async (req, res) => {
+  try {
+    const apartamentosRef = ref(database, "apartamentos");
+    const snapshot = await get(apartamentosRef);
+    if (snapshot.exists()) {
+      const apartamentos = [];
+      snapshot.forEach((childSnapshot) => {
+        apartamentos.push({ id: childSnapshot.key, ...childSnapshot.val() });
+      });
+      res.json(apartamentos);
+    } else {
+      res.status(404).send("Nenhum apartamento encontrado");
+    }
+  } catch (error) {
+    console.error("Erro ao consultar apartamentos:", error);
+    res.status(500).send("Erro interno do servidor");
   }
-  console.log("Conectado ao banco de dados MySQL");
 });
 
-app.get("/apartamentos", (req, res) => {
-  // Consulta SQL para selecionar todos os apartamentos
-  const query = "SELECT * FROM apartamentos";
-
-  // Executa a consulta
-  db.query(query, (err, result) => {
-    if (err) {
-      console.error("Erro ao consultar apartamentos:", err);
-      res.status(500).send("Erro interno do servidor");
-      return;
-    }
-    res.json(result);
-  });
-});
-
-app.get("/apartamentos/:id", (req, res) => {
+app.get("/apartamentos/:id", async (req, res) => {
   const apartamentoId = req.params.id;
-
-  // Consulta SQL para buscar o apartamento pelo ID
-  const query = "SELECT * FROM apartamentos WHERE id = ?";
-
-  // Executa a consulta com o ID fornecido como parâmetro
-  db.query(query, [apartamentoId], (err, result) => {
-    if (err) {
-      console.error("Erro ao buscar apartamento:", err);
-      res.status(500).send("Erro interno do servidor");
-      return;
-    }
-    if (result.length === 0) {
-      // Se nenhum apartamento for encontrado com o ID fornecido, retornar uma resposta 404
+  try {
+    const apartamentoRef = child(
+      ref(database),
+      `apartamentos/${apartamentoId}`
+    );
+    const snapshot = await get(apartamentoRef);
+    if (snapshot.exists()) {
+      res.json({ id: snapshot.key, ...snapshot.val() });
+    } else {
       res.status(404).send("Apartamento não encontrado");
-      return;
     }
-    // Se o apartamento for encontrado, retorná-lo como JSON
-    res.json(result[0]);
-  });
-});
-
-app.post("/apartamentos", (req, res) => {
-  const novoApartamento = req.body;
-
-  // Converter o array de fotos em formato JSON antes de inserir no banco de dados
-  novoApartamento.fotos = JSON.stringify(novoApartamento.fotos);
-
-  // Consulta SQL para inserir um novo apartamento
-  const query =
-    "INSERT INTO apartamentos (nome, preco, regiao, tamanho, endereco, quartos, banheiros, contato, fotos) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-  // Executa a consulta com os valores do novo apartamento
-  db.query(
-    query,
-    [
-      novoApartamento.nome,
-      novoApartamento.preco,
-      novoApartamento.regiao,
-      novoApartamento.tamanho,
-      novoApartamento.endereco,
-      novoApartamento.quartos,
-      novoApartamento.banheiros,
-      novoApartamento.contato,
-      novoApartamento.fotos,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error("Erro ao inserir novo apartamento:", err);
-        res.status(500).send("Erro interno do servidor");
-        return;
-      }
-      novoApartamento.id = result.insertId;
-
-      // Converter a string JSON de volta para um array antes de enviar a resposta
-      novoApartamento.fotos = JSON.parse(novoApartamento.fotos);
-
-      res.status(201).json(novoApartamento);
-    }
-  );
-});
-
-app.put("/apartamentos/:id", (req, res) => {
-  const apartamentoId = req.params.id;
-  const dadosAtualizados = req.body;
-
-  // Converter o array de fotos em formato JSON antes de inserir no banco de dados
-  dadosAtualizados.fotos = JSON.stringify(dadosAtualizados.fotos);
-
-  // Consulta SQL para atualizar o apartamento pelo ID
-  const query = `
-    UPDATE apartamentos
-    SET nome = ?, preco = ?, regiao = ?, tamanho = ?, endereco = ?, quartos = ?, banheiros = ?, contato = ?, fotos = ?
-    WHERE id = ?
-  `;
-
-  // Executa a consulta com os valores atualizados e o ID do apartamento
-  db.query(
-    query,
-    [
-      dadosAtualizados.nome,
-      dadosAtualizados.preco,
-      dadosAtualizados.regiao,
-      dadosAtualizados.tamanho,
-      dadosAtualizados.endereco,
-      dadosAtualizados.quartos,
-      dadosAtualizados.banheiros,
-      dadosAtualizados.contato,
-      dadosAtualizados.fotos,
-      apartamentoId,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error("Erro ao atualizar apartamento:", err);
-        res.status(500).send("Erro interno do servidor");
-        return;
-      }
-
-      if (result.affectedRows === 0) {
-        // Se nenhum apartamento foi atualizado (ID não encontrado), enviar uma resposta 404
-        return res.status(404).send("Apartamento não encontrado");
-      }
-
-      res.status(200).send("Apartamento atualizado com sucesso");
-    }
-  );
-});
-
-app.delete("/apartamentos/:id", (req, res) => {
-  const apartamentoId = req.params.id;
-
-  // Verificar se o ID do apartamento é um número válido
-  if (isNaN(apartamentoId)) {
-    return res.status(400).send("ID do apartamento inválido");
+  } catch (error) {
+    console.error("Erro ao buscar apartamento:", error);
+    res.status(500).send("Erro interno do servidor");
   }
+});
 
-  // Consulta SQL para excluir o apartamento pelo ID
-  const query = "DELETE FROM apartamentos WHERE id = ?";
+app.post("/apartamentos", async (req, res) => {
+  try {
+    const novoApartamento = req.body;
+    const apartamentosRef = ref(database, "apartamentos");
+    const novoApartamentoRef = await push(apartamentosRef);
+    await set(novoApartamentoRef, novoApartamento);
+    res.status(201).json({ id: novoApartamentoRef.key, ...novoApartamento });
+  } catch (error) {
+    console.error("Erro ao inserir novo apartamento:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
+});
 
-  // Executa a consulta com o ID do apartamento
-  db.query(query, [apartamentoId], (err, result) => {
-    if (err) {
-      console.error("Erro ao excluir apartamento:", err);
-      return res.status(500).send("Erro interno do servidor");
+app.put("/apartamentos/:id", async (req, res) => {
+  const apartamentoId = req.params.id;
+  try {
+    const dadosAtualizados = req.body;
+    const apartamentoRef = child(
+      ref(database),
+      `apartamentos/${apartamentoId}`
+    );
+    const snapshot = await get(apartamentoRef);
+    if (snapshot.exists()) {
+      await set(apartamentoRef, dadosAtualizados);
+      res.status(200).send("Apartamento atualizado com sucesso");
+    } else {
+      res.status(404).send("Apartamento não encontrado");
     }
+  } catch (error) {
+    console.error("Erro ao atualizar apartamento:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
+});
 
-    if (result.affectedRows === 0) {
-      // Se nenhum apartamento foi excluído (ID não encontrado), enviar uma resposta 404
-      return res.status(404).send("Apartamento não encontrado");
+app.delete("/apartamentos/:id", async (req, res) => {
+  const apartamentoId = req.params.id;
+  try {
+    const apartamentoRef = child(
+      ref(database),
+      `apartamentos/${apartamentoId}`
+    );
+    const snapshot = await get(apartamentoRef);
+    if (snapshot.exists()) {
+      await remove(apartamentoRef);
+      res.status(200).send("Apartamento excluído com sucesso");
+    } else {
+      res.status(404).send("Apartamento não encontrado");
     }
-
-    res.status(200).send("Apartamento excluído com sucesso");
-  });
+  } catch (error) {
+    console.error("Erro ao excluir apartamento:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 app.listen(port, () => {
